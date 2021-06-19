4652af4a60ceefd6c8248978194a3c620bf62f54
RyutoShimada <business.sub10110@gmail.com>
2021-06-19 19:16:12 +0900

    ContactPointを使うことで法線ベクトルをとってくるのが楽になった。 CollisionExitした時に下後方にRayを飛ばし、その法線と現在の重力方向が違っていたら重力をその法線ベクトルに変えることで、内回りお動きができるようになった

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -11,19 +11,16 @@ public class MoveTest : MonoBehaviour
     [SerializeField] float _fallSpeed = 5;
     [SerializeField] float _gravityPower = 1f;
     [SerializeField] float _maxRayDistance = 1f;
+    [SerializeField] Transform _rayOriginPos = null;
+    [SerializeField] Transform _rotateRayPos = null;
     Rigidbody _rb;
     Vector3 _gravityDir;
     Vector3 _velocity;
     Vector3 _direction;
-    bool _isFall = false;
-    bool _isground = false;
-
-    Ray _forwardRay;
-    Ray _downRay;
-    RaycastHit _forwardHit;
+    RaycastHit _downHit;
 
     /// <summary>Jumpした時にRayを飛ばす距離</summary>
-    const float _jumpRayDis = 1f;
+    const float _jumpRayDis = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -31,23 +28,20 @@ public class MoveTest : MonoBehaviour
         this.gameObject.GetComponent<Rigidbody>().useGravity = false;
         _rb = this.gameObject.GetComponent<Rigidbody>();
         _gravityDir = Vector3.down;
-        _forwardHit = new RaycastHit();
     }
 
     void FixedUpdate()
     {
         Move(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Jump(_jumpPower);
-        IsFall();
-        IsGround();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _downRay = new Ray(transform.position, _gravityDir);
-        Physics.Raycast(_forwardRay, out _forwardHit, _maxRayDistance);
-        Debug.DrawRay(_downRay.origin, _downRay.direction * _jumpRayDis, Color.blue, 0, false);
+        Physics.Raycast(_rayOriginPos.position, _rotateRayPos.position - _rayOriginPos.position, out _downHit, _maxRayDistance * 2);
+        Debug.DrawRay(_rayOriginPos.position, (_rotateRayPos.position - _rayOriginPos.position).normalized * _maxRayDistance * 2, Color.green, 0, false);
+        Jump(_jumpPower);
+        IsFall();
     }
 
     void Move(float h, float v)
@@ -63,6 +57,14 @@ public class MoveTest : MonoBehaviour
             if (_gravityDir == Vector3.up || _gravityDir == Vector3.down)
             {
                 _velocity.y = _rb.velocity.y;
+            }
+            else if (_gravityDir == Vector3.left || _gravityDir == Vector3.right)
+            {
+                _velocity.x = _rb.velocity.x;
+            }
+            else if (_gravityDir == Vector3.forward || _gravityDir == Vector3.down)
+            {
+                _velocity.z = _rb.velocity.z;
             }
 
             _rb.velocity = _velocity;
@@ -92,38 +94,34 @@ public class MoveTest : MonoBehaviour
     /// <param name="jumpPower">ジャンプする力</param>
     void Jump(float jumpPower)
     {
-        if (Input.GetButtonDown("Jump") && _isground)
+        if (Input.GetButtonDown("Jump"))
         {
-            _rb.AddForce(-_gravityDir.normalized * jumpPower, ForceMode.Impulse);
+            if (IsGround())
+            {
+                _rb.AddForce(-_gravityDir.normalized * jumpPower, ForceMode.Impulse);
+            }
         }
     }
 
-    void IsGround()
+    bool IsGround()
     {
-        if (Physics.Raycast(_downRay, _jumpRayDis))
+        if (Physics.Raycast(transform.position, _gravityDir, _jumpRayDis))
         {
-            _isground = true;
+            return  true;
         }
         else
         {
-            _isground = false;
             _gravityDir = Vector3.down;
+            return  false;
         }
     }
 
     void IsFall()
     {
-        if (_rb.velocity.y < 0 && !_isground)
+        if (_rb.velocity.y < 0 && !IsGround())
         {
-            _isFall = true;
             _gravityDir = Vector3.down;
-            Vector3 v3 = _rb.velocity;
-            v3.y = -_fallSpeed;
-            _rb.velocity = v3;
-        }
-        else
-        {
-            _isFall = false;
+            _rb.AddForce(_gravityDir * _fallSpeed, ForceMode.Force);
         }
     }
 
@@ -138,25 +136,22 @@ public class MoveTest : MonoBehaviour
         //https://teratail.com/questions/290578
         Quaternion toRotate = Quaternion.FromToRotation(transform.up, nomal) * transform.rotation;
         transform.rotation = toRotate;
+        _rb.AddForce(_gravityDir * _gravityPower, ForceMode.Impulse);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        //if (collision.collider.tag == "Floor")
-        //{
-        //    ChangeRotate(Vector3.up);
-        //}
-
         foreach (ContactPoint point in collision.contacts)
         {
             ChangeGravity(point.normal);
         }
+    }
 
-        //if (!_forwardHit.collider) return;
-
-        //if (_forwardHit.collider.name == collision.collider.name)
-        //{
-        //    ChangeGravity(_forwardHit.normal);
-        //}
+    private void OnCollisionExit(Collision collision)
+    {
+        if (_downHit.normal != -_gravityDir)
+        {
+            ChangeGravity(_downHit.normal);
+        }
     }
 }
