66d0cc534e6361c8e4be8d5089d83587eab56ea0
RyutoShimada <business.sub10110@gmail.com>
2021-07-06 20:27:02 +0900

    Eventシステムの勉強をするために実験用のスクリプトを作成

---
+++ Assets/Scripts/EventSystemsTarget.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystemsTarget : MonoBehaviour, IEventCaller
+{
+    /// <Summary>
+    /// EventSystemsを使用してこのメソッドを呼び出します。
+    /// </Summary>
+    public void EventCall()
+    {
+        // ログを表示します。
+        Debug.Log("EventSystemsによるイベントが通知された！");
+        Debug.Log("ここに実行するメソッドを書いておくと呼ばれる");
+    }
+}
---
+++ Assets/Scripts/IEventCaller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public interface IEventCaller : IEventSystemHandler
+{
+    // イベントを呼び出すメソッドです。
+    void EventCall();
+}
