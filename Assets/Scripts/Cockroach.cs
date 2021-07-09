9a5943d3713f0ea45ee98b03fc68703d5ff12157
RyutoShimada <business.sub10110@gmail.com>
2021-07-09 12:49:52 +0900

    1秒おきに体力が減少し、食べ物を食べると回復し、攻撃を食らうとダメージを受ける 体力はAX値を超えず、0未満にならない 体力が0以下になるとDedフラグが立つ

--- Assets/Scripts/Cockroach.cs
+++ Assets/Scripts/Cockroach.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class Cockroach : MonoBehaviour
 {
-    /// <summary>体力</summary>
-    [SerializeField] int m_hp = 100;
+    /// <summary>最大の体力値</summary>
+    [SerializeField] int m_maxHp = 100;
     /// <summary>満腹ゲージ</summary>
     [SerializeField] int m_satietyGauge = 100;
     /// <summary>満腹ゲージが1秒間に減少する値</summary>
-    [SerializeField] int m_decreaseValue = 1;
+    [SerializeField] int m_decreaseValueIn1second = 1;
+    /// <summary>現在の体力値</summary>
+    int m_currentHp;
+    /// <summary>死んだかどうか</summary>
+    bool m_isDed = false;
+    float m_timer = 0;
+
+    private void Start()
+    {
+        m_currentHp = m_maxHp;
+    }
+
+    private void Update()
+    {
+        DecreaseHitPoint(m_decreaseValueIn1second);
+    }
+
+    /// <summary>
+    /// 1秒おきにHitPointを減らす
+    /// </summary>
+    /// /// <param name="decreaseValue">減少させる量</param>
+    void DecreaseHitPoint(int decreaseValue)
+    {
+        m_timer += Time.deltaTime;
+
+        if (m_timer >= 1f)
+        {
+            m_timer = 0;
+            m_currentHp -= decreaseValue;
+            Debug.Log("HP : " + m_currentHp);
+        }
+    }
 
     /// <summary>
     /// 食べ物を食べて、満腹ゲージを回復する。
@@ -17,7 +48,15 @@ public class Cockroach : MonoBehaviour
     /// <param name="heelValue">体力に加算する値</param>
     void Eat(int heelValue)
     {
-        m_hp += heelValue;
+        m_currentHp += heelValue;
+
+        // 現在のHPがHPの最大値を超えないようにする
+        if (m_currentHp > m_maxHp)
+        {
+            m_currentHp = m_maxHp;
+        }
+
+        Debug.Log("Heel");
     }
 
     /// <summary>
@@ -26,14 +65,23 @@ public class Cockroach : MonoBehaviour
     /// <param name="damageValue">体力を減算する値</param>
     public void BeAttacked(int damageValue)
     {
-        m_hp -= damageValue;
+        m_currentHp -= damageValue;
+        Debug.Log($"Hit!Damage! -{damageValue}, HP : {m_currentHp}");
+        if (m_currentHp < 0)
+        {
+            m_currentHp = 0;
+            m_isDed = true;
+            Debug.Log("Ded!");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Food")
         {
-            // Eat(other.gameObject.GetComponent<Food>().m_heelValue);
+            Eat(other.gameObject.GetComponent<Food>().m_heelValue);
+            Debug.Log($"Eat! +{other.gameObject.GetComponent<Food>().m_heelValue}, HP : {m_currentHp}");
+            other.gameObject.GetComponent<Food>().UnActive();
         }
     }
 }
