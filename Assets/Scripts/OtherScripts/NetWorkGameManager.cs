573169ddbf02b3dd463f37a05734b0e94c699c0a
RyutoShimada <business.sub10110@gmail.com>
2021-07-20 20:35:32 +0900

    フォルダの移動

--- Assets/Scripts/OtherScripts/NetWorkGameManager.cs
+++ Assets/Scripts/OtherScripts/NetWorkGameManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
 
-public class NetWorkGameManager : MonoBehaviour
+namespace Photon.Pun.Demo.PunBasics
 {
-    // Start is called before the first frame update
-    void Start()
+    public class NetWorkGameManager : MonoBehaviourPunCallbacks
     {
-        
-    }
+        #region Public Fields
 
-    // Update is called once per frame
-    void Update()
-    {
+        static public GameManager m_Instance;
+
+        #endregion
+
+        #region Private Fields
+
+        private GameObject m_instance;
+
+        [Tooltip("CockroachNetWorkのPrefab")]
+        [SerializeField]
+        private GameObject m_cockroachPrefab;
+
+        [Tooltip("HumanNetWorkのPrefab")]
+        [SerializeField]
+        private GameObject m_humanPrefab;
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        void Start()
+        {
+
+        }
+
         
+        void Update()
+        {
+
+        }
+
+        #endregion
     }
 }
