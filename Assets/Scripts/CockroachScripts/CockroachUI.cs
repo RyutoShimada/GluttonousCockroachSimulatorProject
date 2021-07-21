17a17644685dd35862a731622748a27051a230ca
RyutoShimada <business.sub10110@gmail.com>
2021-07-22 01:55:55 +0900

    ゴキブリのUIをGameManagerTestSceneではhierarchyに存在しているオブジェクトを参照してコントロールしていたが、そうではなくPrefabから参照してインスタンス化するように変更した。 Canvasさえあれば、どのシーンにゴキブリを生成してもUIが表示される

--- Assets/Scripts/CockroachScripts/CockroachUI.cs
+++ Assets/Scripts/CockroachScripts/CockroachUI.cs
@@ -10,11 +10,14 @@ using DG.Tweening;
 public class CockroachUI : MonoBehaviour
 {
     /// <summary>満腹ゲージのUI</summary>
-    [SerializeField] Image m_satietyGaugeImage = null;
+    Image m_satietyGaugeImage = null;
     /// <summary>HPバーのUI</summary>
-    [SerializeField] Slider m_hpSlider = null;
+    Slider m_hpSlider = null;
     /// <summary>ダメージを受けた時のUI</summary>
-    [SerializeField] Image m_damageImage = null;
+    Image m_damageImage = null;
+
+    [SerializeField] GameObject m_cockroachUiPrefab = null;
+
     /// <summary>UIの動きを何秒かけて行うか</summary>
     [SerializeField, Range(0.1f, 1.0f)] float m_afterSeconds = 0.2f;
     /// <summary>m_damageImageの初期colorを保存しておく変数</summary>
@@ -24,13 +27,42 @@ public class CockroachUI : MonoBehaviour
 
     private void Start()
     {
-        m_damageImage.gameObject.SetActive(true);
-        m_originDamageColor = m_damageImage.color;
+        if (!m_cockroachUiPrefab)
+        {
+            Debug.LogError("m_cockroachUiPrefab が NULL です。CockroachUI スクリプトに cockroachUiPrefab がアサインされているか確認して下さい。", this);
+        }
+        else
+        {
+            Transform t = GameObject.Find("Canvas").transform;
+            GameObject go = Instantiate(m_cockroachUiPrefab, t);
 
-        // アルファ値を0にする
-        m_color = m_damageImage.color;
-        m_color.a = 0;
-        m_damageImage.color = m_color;
+            if (go)
+            {
+                m_satietyGaugeImage = m_cockroachUiPrefab.transform.Find("Gauge").GetComponentInChildren<Image>();
+                m_hpSlider = m_cockroachUiPrefab.transform.Find("HPSlider").GetComponent<Slider>();
+                m_damageImage = m_cockroachUiPrefab.transform.Find("DamageImage").GetComponent<Image>();
+            }
+            else
+            {
+                Debug.LogError("m_cockroachUiPrefab がインスタンス化されていません。", this);
+            }
+            
+
+            if (m_damageImage)
+            {
+                m_damageImage.gameObject.SetActive(true);
+                m_originDamageColor = m_damageImage.color;
+
+                // アルファ値を0にする
+                m_color = m_damageImage.color;
+                m_color.a = 0;
+                m_damageImage.color = m_color;
+            }
+            else
+            {
+                Debug.LogError("m_damageImage を GetComponent 出来ませんでした。", this);
+            }
+        }
     }
 
     /// <summary>
