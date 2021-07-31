eeb28c04c70a8be6e1c31de7e4d2e60e2d64d0c6
RyutoShimada <business.sub10110@gmail.com>
2021-07-31 17:46:37 +0900

    フォルダの移動

--- Assets/Scripts/OtherScripts/InitializeScreen.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InitializeScreen
-{
-    [RuntimeInitializeOnLoadMethod]
-    static void OnRuntimeMethodLoad() => Screen.SetResolution(1280, 720, false);
-}
