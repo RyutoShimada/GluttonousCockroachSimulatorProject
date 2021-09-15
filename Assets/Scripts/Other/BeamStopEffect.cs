2185cc9d5ed522d073cf13409b0cb5cd1d9627ab
RyutoShimada <business.sub10110@gmail.com>
2021-09-16 02:12:20 +0900

    ビームが当たったおころで止まるようにし、当たっている所に出すエフェクトを作った

---
+++ Assets/Scripts/Other/BeamStopEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamStopEffect : MonoBehaviour
+{
+    [SerializeField] GameObject m_stopEffect = null;
+    RaycastHit m_hit;
+    float m_distance;
+
+    private void Start()
+    {
+        m_distance = GetComponent<BoxCollider>().size.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out m_hit, m_distance))
+        {
+            if (!m_stopEffect.activeSelf) m_stopEffect.SetActive(true);
+            if (m_hit.collider.tag == "Attack") return;
+            m_stopEffect.transform.position = m_hit.point;
+        }
+    }
+}
